c3f0f82fd2768acfce0253bc9680b3185be222f2
tomatodayo <kantogoto3104@gmail.com>
2022-02-06 22:22:17 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
@@ -47,14 +48,18 @@ class PrototypesController < ApplicationController
     end
   end
 
-  
+
   private
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
  
-  def contributor_confirmation
+  def move_to_index
     redirect_to root_path unless current_user == @prototype.user
   end
 
